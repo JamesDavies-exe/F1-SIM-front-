47608ab4237e7d40393b1ffbd0b98e2bb429b991
James <jamesaguilarf@gmail.com>
2024-06-19 23:48:01 +0200

    Landing page mejorado

--- f1-sim/src/components/Home.jsx
+++ f1-sim/src/components/Home.jsx
@@ -2,40 +2,30 @@ import "./style/home.css";
 import "./fonts/Formula1-Bold_web.ttf";
 import Navbar from "./Navbar"
 import { Box, Typography, Button } from '@mui/material';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import Login from "./Login";
 function Home() {
   const navigate = useNavigate();
   return (
     <>
-    <Navbar></Navbar>
     <div className="video-background">
         <video autoPlay loop muted>
           <source src="/film.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-        <Box
-      sx={{
-        height: '90vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: 'black',
-        textAlign: 'center',
-        p: 2,
-      }}
-    >
-      <Typography variant="h2" component="h1" gutterBottom>
-        FORMULA ONE QUIZ
-      </Typography>
-      <Typography variant="h5" component="h2" gutterBottom>
-        Descubre tu conocimiento sobre la Formula 1 y compite para llegar a lo más alto
-      </Typography>
-      <Button variant="contained" color="primary" size="large" onClick={() => navigate("/circuit")}>
-        Jugar
-      </Button>
-    </Box>
+
+      <section class="showcase">
+    <div class="overlay"></div>
+    <div class="text">
+      <h3>Formula One Quiz</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat.</p>
+      <Link to={"/login"}>Empezar</Link>
+    </div>
+  </section>
     </>
   );
 }
